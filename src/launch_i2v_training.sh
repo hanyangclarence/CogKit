c380355d4b15db79aeaac345108f116cdc328f22
hanyangclarence <mahlerrrr76@gmail.com>
2025-04-06 21:03:44 +0800

    update

--- src/launch_i2v_training.sh
+++ src/launch_i2v_training.sh
@@ -46,7 +46,7 @@ OUTPUT_ARGS=(
 
 # Data Configuration
 DATA_ARGS=(
-    --data_root "/gpfs/u/home/LMCG/LMCGhazh/scratch/enroot/rlbench_data/root/RACER-DataGen/racer_datagen/rlbench_videos"
+    --data_root "/gpfs/u/scratch/LMCG/LMCGhazh/enroot/rlbench_data/root/RACER-DataGen/racer_datagen/rlbench_videos"
 
     # Note:
     #  for CogVideoX series models, number of training frames should be **8N+1**
