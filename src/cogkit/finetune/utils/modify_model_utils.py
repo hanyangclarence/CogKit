bfe2af7999eb26419f9bc95cc4a2422c9c618660
hanyangclarence <mahlerrrr76@gmail.com>
2025-05-24 10:42:59 +0800

    update

--- src/cogkit/finetune/utils/modify_model_utils.py
+++ src/cogkit/finetune/utils/modify_model_utils.py
@@ -4,10 +4,7 @@ from torch.nn.modules.conv import Conv2d
 
 def modify_in_conv(module: nn.Module) -> nn.Module:
     old_conv = module.transformer.transformer.patch_embed.proj
-    
-    print(f"!!!! in_features: {old_conv.in_features}, out_features: {old_conv.out_features}")
-    print(f"!!! old_conv: {old_conv}")
-    
+
     assert isinstance(old_conv, Conv2d), f"Expected Conv2d, got {type(old_conv)}"
     assert old_conv.in_channels == 32, f"Expected in_channels=32, got {old_conv.in_channels}"
     
@@ -31,4 +28,24 @@ def modify_in_conv(module: nn.Module) -> nn.Module:
         new_conv.weight[:, old_conv.in_channels:, :, :] = 0.0
     module.transformer.transformer.patch_embed.proj = new_conv
     return module
+
+
+def modify_in_linear(module: nn.Module) -> nn.Module:
+    old_linear = module.transformer.transformer.patch_embed.proj
+    assert isinstance(old_linear, nn.Linear), f"Expected Linear, got {type(old_linear)}"
+    assert old_linear.in_features == 256, f"Expected in_features=32, got {old_linear.in_features}"
     
+    new_linear = nn.Linear(
+        in_features=384,
+        out_features=old_linear.out_features,
+        bias=old_linear.bias is not None,
+    )
+    
+    # change the pretrained model's in_features to 384
+    with torch.no_grad():
+        new_linear.weight[:, :old_linear.in_features] = old_linear.weight.clone()
+        if old_linear.bias is not None:
+            new_linear.bias.data = old_linear.bias.clone()
+        new_linear.weight[:, old_linear.in_features:] = 0.0
+    module.transformer.transformer.patch_embed.proj = new_linear
+    return module
